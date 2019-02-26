5131809d616f2778d4c0625b9eb81eef88958d73
maocat <zk246422@163.com>
2019-02-26 10:05:12 +0800

    ...

--- html温故知新/javescript/web_api/static/js/common.js
+++ html温故知新/javescript/web_api/static/js/common.js
@@ -5,3 +5,12 @@
 function my$(id){
     return document.getElementById(id)
 }
+
+/**
+ * 
+ * @param {*} className dom 对象的class属性
+ * 返回list对象
+ */
+function myc$(className){
+    return document.getElementsByClassName(className)
+}
