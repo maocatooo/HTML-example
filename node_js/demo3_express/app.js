0a33dfd5ca4ec6c3aaea86b630a235c33780591a
maocat <zk246422@163.com>
2019-03-20 15:59:17 +0800

    callback 保存数据

--- node_js/demo3_express/app.js
+++ node_js/demo3_express/app.js
@@ -2,70 +2,111 @@ var experss = require('express')
 
 var body = require('body-parser')
 var fs = require('fs')
-var r = require('./index')
-console.log(r)
+// var r = require('./index')
+// console.log(r)
 var app1 = experss()
+
 // 静态文件，开放目录
-function print(p){
+function print(p) {
     console.log(p)
 }
-app1.use('/static/',experss.static('./static'))
+
+app1.use('/static/', experss.static('./static'))
 // 使用body 可以获取表单提交的内容
-app1.use(body.urlencoded({ extended:false }))
+app1.use(body.urlencoded({extended: false}))
 
-function readFile(callback){
-    fs.readFile('./data/db.json',function(error,data){
-        if(error){
+function readFile(callback) {
+    fs.readFile('./data/db.json', function (error, data) {
+        if (error) {
             callback(error)
-        }
-        else{
-            callback()
+        } else {
+            callback(null, JSON.parse(data.toString()))
         }
     })
-        return JSON.parse(fs.readFileSync())
 }
-function saveFile(data){
-    fs.writeFile('./data/db.json',JSON.stringify(data),function(error){
-        if(error){
-            print(error)
-        }else{
-        }
 
+function saveFile(query, callback) {
+    console.log(query)
+    fs.readFile('./data/db.json', function (error, data) {
+        if (error) {
+            callback(error)
+        } else {
+            var datas = JSON.parse(data.toString())
+            console.log(datas)
+            datas.push({user: query})
+            console.log(datas)
+            fs.writeFile('./data/db.json', JSON.stringify(datas), function (error) {
+                if (error) {
+                    callback(error)
+                } else {
+                    callback(null, datas)
+                }
+
+            })
+        }
     })
+
 }
-// 配置使用 art-template 
+
+// 配置使用 art-template
 // 默认回去views目录底下去找模板文件 html 以.html结尾的文件
 // 如果要修改目录 app1.set('views','./aa')
 app1.engine('html', require('express-art-template'))
 // app1.set('views','./aa')
-app1.get('/',function(request,response){
+app1.get('/', function (request, response) {
     response.render('index.html')
     // response.send("hello world")
 })
-app1.get('/post',function(request,response){
-    console.log(readFile())
+app1.get('/post', function (request, response) {
+    console.log(123)
     var query = request.query
-    print(query)
-    var users =  readFile()
+
+    // var users =  readFile()
     if (JSON.stringify(query) !== "{}") {
-       
-       users.push({user:query})
+        saveFile(query, function (error, data) {
+            if (error) {
+                response.send('404 not found!',)
+            } else {
+                response.render('post/post.html', {users: data})
+            }
+        })
+        // users.push({user:query})
+    } else {
+        readFile(function (error, data) {
+            if (error) {
+                response.send('404 not found!',)
+            } else {
+                response.render('post/post.html', {users: data})
+            }
+        })
     }
-    response.render('post/post.html', {users:users})
+
     // response.send("hello world")
 })
-app1.post('/post',function(request,response){
-    
+app1.post('/post', function (request, response) {
+
     var query = request.body
     if (JSON.stringify(query) !== "{}") {
-        var users = readFile()
-        users.unshift({user:query})
-        saveFile(users)
+        saveFile(query, function (error, data) {
+            if (error) {
+                response.send('404 not found!',)
+            } else {
+                response.redirect('/post')
+            }
+        })
+        // users.push({user:query})
+    } else {
+        readFile(function (error, data) {
+            if (error) {
+                response.send('404 not found!',)
+            } else {
+                response.redirect('/post')
+            }
+        })
     }
-    response.redirect('/post')
     // response.send("hello world")
 })
-app1.use(r)
-app1.listen(3000,function(){
+// app1.use(r)
+app1.listen(3000, function () {
     console.log('running')
 })
