3eab685c42e56cc770c4087e776d0f260305c970
maocat <zk246422@163.com>
2019-03-21 18:04:50 +0800

    mongo

---
+++ node_js/demo4_mongo/app.js
@@ -0,0 +1,13 @@
+var mongo = require('mongodb').MongoClient;
+var url = "mongodb://mongo:27017/";
+
+mongo.connect(url, { useNewUrlParser: true }, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("runoob");
+    var myobj = { name: "123", url: "www.bss" };
+    dbo.collection("site").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("文档插入成功");
+        db.close();
+    });
+});
