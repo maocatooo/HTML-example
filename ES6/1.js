b5afb9a3c567478bc4a4a672cea604d831372803
MAODOU <zk246422@163.com>
2019-03-24 23:49:16 +0800

    ...

---
+++ ES6/1.js
@@ -0,0 +1,66 @@
+// let a = 1
+// {
+//     let b = 2
+//     console.log(a) //1
+// }
+// console.log(b) // ReferenceError:b is not defined
+
+//不存在变量提升
+// // var 的情况
+// console.log(foo); // 输出undefined
+// var foo = 2;
+//
+// // let 的情况
+// console.log(bar); // 报错ReferenceError
+// let bar = 2;
+
+// let a = 10;
+// let a = 1; //报错 不允许重复声明
+
+// let a = 10;
+// var a = 1;//报错 不允许重复声明
+
+
+// function f() { console.log('I am outside!'); }
+//
+// (function () {
+//     if (false) {
+//         // 重复声明一次函数f
+//         function f() { console.log('I am inside!'); }
+//     }
+//
+//     f();
+// }());
+
+// //const
+// const foo = {};
+//
+// // 为 foo 添加一个属性，可以成功
+// foo.prop = 123;
+// foo.prop // 123
+//
+// // 将 foo 指向另一个对象，就会报错
+// foo = {}; // TypeError: "foo" is read-only
+
+
+// // 解构赋值
+// let [foo, [[bar], baz]] = [1, [[2], 3]];
+// foo // 1
+// bar // 2
+// baz // 3
+//
+// let [ , , third] = ["foo", "bar", "baz"];
+// third // "baz"
+//
+// let [x, , y] = [1, 2, 3];
+// x // 1
+// y // 3
+//
+// let [head, ...tail] = [1, 2, 3, 4];
+// head // 1
+// tail // [2, 3, 4]
+//
+// let [x, y, ...z] = ['a'];
+// x // "a"
+// y // undefined
+// z // []
---
+++ TS/1.ts
@@ -0,0 +1,22 @@
+let str:string = "123"
+
+
+// string
+// boolean
+// number
+// any  任意类型
+// Tuple 元组
+let t:[string,number]=['1',1]
+
+// array
+let arr: string[] = ['1','2']
+let arr1: any[] = ['1','2',1]
+
+// 枚举
+enum Color {
+    Red,
+    Green,
+    Blue
+};
+
+let c: Color = Color.Red; // 0
