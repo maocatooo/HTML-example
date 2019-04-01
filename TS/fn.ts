b37628eccd3e645a038ad61c5c9424c93807a292
maocat <zk246422@163.com>
2019-04-01 11:08:14 +0800

    .....

---
+++ TS/fn.ts
@@ -0,0 +1,21 @@
+//
+// // 无返回值
+// function run():void {
+//
+// }
+// // ? 可选参数
+// function run1(name:string, age?:number):string {
+//     return `${name}--${age}`
+// }
+// alert(run1("123e",111))
+
+// 默认参数 必须配置到最后面
+// function fn1(name:string, age:number=1):string {
+//     return `${name}--${age}`
+// }
+// alert(fn1("123e"))、
+// 剩余参数
+
+function fn1(num1:number):number {
+    return
+}
